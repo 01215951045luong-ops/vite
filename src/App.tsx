55f84555e62b1f931acceb9f6cdb8dfe993a1007
Liang <01215951045luong@gmail.com>
2025-11-27 15:58:12 +0800

    CSS

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Layout from './components/Layout'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+  {/*const [count, setCount] = useState(0)*/}
+
+const signList = ["%", "CE", "C", "X", 
+                        "1/x", "x2", "2√x", "÷",
+                         "7", "8", "9", "×", 
+                         "4", "5", "6", "-", 
+                         "1", "2", "3", "+",
+                          "+/-", "0", ".", "="];
+const [count, setCount] = useState(0)
   return (
     <Layout>
-      <div className="grid grid-cols-4 gap-4">
-      {/* 這裡面的子元素會自動變成 3 欄排列 */}
+      {/*<div className="grid grid-cols-4 gap-4">
+       這裡面的子元素會自動變成 3 欄排列 
       <div className='rounded-md border-2'>%</div>
       <div className='rounded-md border-2'>CE</div>
       <div className='rounded-md border-2'>C</div>
@@ -40,7 +48,23 @@ function App() {
       <div className='rounded-md border-2'>0</div>
       <div className='rounded-md border-2'>.</div>
       <div className='rounded-md border-2'>=</div>
-</div>
+</div>*/}
+    <div className="max-w-sm mx-auto p-4 border-2 border-gray-300 rounded-lg shadow-md">
+    <div className="grid grid-cols-4 gap-4">
+    {
+    signList.map(
+      (sign,index) => { 
+        return(
+          <div key={index} className="rounded-md border-2 flex items-center justify-center bg-gray-200 p-4 text-center cursor-pointer hover:bg-gray-300 transition">
+           {sign}
+          </div>
+          )
+        }
+      )
+      
+}
+    </div>
+  </div>
     </Layout>
   )
 }
--- src/MyView.tsx
+++ src/MyView.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 
-
-
+interface Pet {
+  id: number;
+  title: string;
+  name: string;
+}
+const pets: Pet[] = [
+  { id: 101, title: "dog", name: "Lucky" },
+  { id: 102, title: "cat", name: "kitty" },
+];
 export const MyView: React.FC = () => {
 
 
   return (
-    <div className="fixed inset-0 bg-opacity-80 flex items-center justify-center z-50 transition-opacity duration-300">
-hi
+     <div className="p-4 border rounded">
+      <h3>Fruit List</h3>
+      <ul>
+        {/* map 的用法不變，但現在 TS 知道 'fruit' 是一個 string */}
+        {pets.map((pet) => (
+          <div key={pet.id}>
+          <span className="font-semibold text-lg">{pet.title}</span>
+          <input  className="p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" type="text" value={pet.name}/>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 };
